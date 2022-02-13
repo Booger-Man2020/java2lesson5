984391b09867d7d2437636c0ca9bf488b945de24
Aleh <vatsevicho@gmail.com>
2022-02-13 16:51:37 +0300

    Метод1 бежит по массиву и вычисляет значения . выводит время работы метода Метод2 разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает эти массивы обратно в один. вы водит время работы метода

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,74 @@
 import java.util.Arrays;
 
 public class Main {
-    static final int size= 1000000;
-    static final int size2=size/2;
+    static final int size = 1_000_000;
+    static final int size2 = size / 2;
 
 
     public static void main(String[] args) {
         setArr();
-        Thread threadSetNewArray =new Thread();
+        // System.out.println();
+        treads();
+
 
     }
 
-
-    public static void setArr(){
+    public static void setArr() {
         float[] arr = new float[size];
         Arrays.fill(arr, 1.0F);
         //System.out.println(Arrays.toString(arr));
         long a = System.currentTimeMillis();
-        for (int i=0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
+        System.out.print("Время работы первого метода: ");
         System.out.println(System.currentTimeMillis() - a);
-        System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(arr));
     }
 
-    public static class ThreadSetNewArray () extends Thread {
+    public static void treads() {
         float[] arr = new float[size];
         Arrays.fill(arr, 1.0F);
-        float [] arr1=new float[size2];
-        System.arraycopy(arr, 0, arr1, 0,size2);
-        System.out.println(Arrays.toString( arr));
+        float[] arr1 = new float[size2];
+        float[] arr2 = new float[size2];
+        System.arraycopy(arr, 0, arr1, 0, size2);
+        System.arraycopy(arr, size2, arr2, 0, size2);
+        long a = System.currentTimeMillis();
 
+
+        Thread newArray = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < size2; i++) {
+                    arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                    //  System.out.println(Arrays.toString(arr1));
+                }
+            }
+        });
+        newArray.start();
+
+        Thread MThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < size2; i++) {
+                    arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
+                }
+
+            }
+
+        });
+        MThread.start();
+        try {
+            newArray.join();
+            MThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.arraycopy(arr1, 0, arr, 0, size2);
+        System.arraycopy(arr2, 0, arr, size2, size2);
+        // System.out.println(Arrays.toString(arr));
+        System.out.print("Время работы второго метода: ");
+        System.out.println(System.currentTimeMillis() - a);
     }
 }
